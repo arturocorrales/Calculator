7b995918448551897ae9b5ec5affd85be10b6338
arturocorrales <arturocorrales@live.com>
2016-02-20 23:07:19 -0700

    Initial commit of Calculator.

---
+++ CSE 360/src/cse360assign3/Calculator.java
@@ -0,0 +1,63 @@
+package cse360assign3;
+
+/**
+ * 
+ * @author Arturo Corrales CSE 360 PIN: 221
+ */
+public class Calculator {
+
+	private int total;
+	
+	/** Constructor for Calculator*/
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns total
+	 * @return
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds value to total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts value to total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies value to total
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides value to total
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns history
+	 * @return
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
